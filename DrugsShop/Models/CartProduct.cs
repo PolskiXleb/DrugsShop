1f1d5ddfdfe348aff655b417e4e7906f306fa27a
Yakov <39483038+PolskiXleb@users.noreply.github.com>
2018-05-30 15:10:22 +0300

    Alot
    js scripts, cart, adding products on product page, storing theirs amount in localstore

---
+++ DrugsShop/Models/CartProduct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DrugsShop.Models
+{
+    public class CartProduct
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Cost { get; set; }
+
+        public bool ByRecipe { get; set; }
+
+        public int Amount { get; set; }
+
+        public int Sum { get; set; }
+    }
+}
--- DrugsShop/Scripts/cart.js
+++ DrugsShop/Scripts/cart.js
@@ -1,21 +1,101 @@
-﻿
-function addToCart() {
-    var button = document.getElementById("AddToCart");
-    //button.addEventListener("click", clickAdd(button.value))
+﻿$(document).ready(
+    loadCart
+);
+
+function loadCart() {
+    var products = document.getElementsByClassName("count");
+    for (var p = 0; p < products.length; p++) {
+        var textbox = products[p];
+        var amount = localStorage[textbox.attributes["id"].value];
+
+        if (amount != null)
+            textbox.value = amount;
+        else
+            textbox.value = "0";
+    }
 }
 
 function clickAdd(id) {
     var textbox = document.getElementById(id);
-    textbox.value = Number(textbox.value) + Number(1);
+    textbox.value = +textbox.value + +1;
+    localStorage[id] = textbox.value;
 }
 
-function updateCart(id) {
-    var textbox = document.getElementById(id);
-    var amount = sessionStorage[id];
-    if (amount == null)
-        textbox.value = 0;
+function getCartId() {
+    var id = [];
+    for (var i = 0; i < localStorage.length; i++)
+    {
+        if (i > 0) id.push(i);
+    }
+    return id;
+}
+
+function getCartAmount() {
+    var amount = [];
+    for (var i = 0; i < localStorage.length; i++) {
+        if (i > 0) amount.push(localStorage[i]);
+    }
+    return amount;
+}
+
+//function openCart() {
+//    var amount = [];
+//    var id = [];
+
+//    for (var i = 0; i < localStorage.length; i++) {
+//        if (i > 0) id.push(localStorage.);
+//        if (i > 0) amount.push(localStorage[i]);
+//    }
+
+//    var url = 'Cart/GetCart' + '?id=' + id + '&amount=' + amount;
+//    window.location.replace(url);
+//}
+
+function getProducts() {
+    var products = [];
+    var product;
+    var id;
+    var amount;
+
+    for (var key in localStorage) {
+        id = key;
+        if (id > 0) {
+            amount = localStorage[key];
+            product = { "Id": id, "Amount": amount };
+            products.push(product);
+        }
+    }
+
+    return products
+}
+
+function openCart() {
+    var products = getProducts();
+
+    $.ajax({
+        //url: 'Cart/GetCart',
+        url: '@(Url.Action("Cart", "GetCart"))',
+        type: 'POST',
+        data: JSON.stringify(products),
+        contentType: "application/json;charset=utf-8"
+    });
+}
+
+function checkout(status) {
+    if (status == 2)
+    {
+        var products = getProducts();
+
+        $.ajax({
+            url: 'Cart/Checkout',
+            type: 'POST',
+            data: JSON.stringify(products),
+            contentType: "application/json;charset=utf-8"
+        });
+    }
     else
-        textbox.value = amount;
+    {
+        var url = 'User/Login'
+        window.location.replace(url);
+    }
 }
-
-
